beb30b7c0fd737e779b56adb6fe3866a5153ca3e
menisy <elmenisy@gmail.com>
2018-05-03 18:08:07 +0200

    Added table printing

--- prime_tests.rb
+++ prime_tests.rb
@@ -9,20 +9,19 @@ class PrimesTests < Assert::Context
   # Compare prime generation with built in
   # ruby prime class
   def test_prime_generation
-    assert_equal PrimesGenerator.n_primes(10),
-                  Prime.first(10)
-    assert_equal PrimesGenerator.n_primes(20),
-                  Prime.first(20)
-    assert_equal PrimesGenerator.n_primes(200),
-                  Prime.first(200)
+    arr = [10, 20, 50, 1000]
+    arr.each do |i|
+      assert_equal PrimesGenerator.n_primes(i),
+                  Prime.first(i)
+    end
   end
 
 
   def test_benchmarks
     Benchmark.bm do |bm|
-      puts "My Prime Generator"
+      puts "My Prime Generator for n = 1000"
       bm.report{PrimesGenerator.n_primes(1000)}
-      puts "Ruby's Prime Generator"
+      puts "Ruby's Prime Generator for n = 1000"
       bm.report{Prime.first(1000)}
     end
   end
---
+++ primes_mult_table.rb
@@ -0,0 +1,44 @@
+require_relative 'primes_generator'
+
+module PrimesMultTable
+
+  def self.generate_table(primes=[])
+
+    n = primes.length
+    table = Array.new(n){ Array.new(n) }
+
+    for i in (0...n)
+      for j in (i...n)
+        mult = primes[i] * primes[j]
+        table[i][j], table[j][i] = mult, mult
+      end
+    end
+    table
+  end
+
+  def self.print_table(arg)
+    n = arg ? arg.to_i : 10
+
+    return puts "Invalid n, please use n > 1" if n < 1
+
+    primes = PrimesGenerator.n_primes(n)
+    table = generate_table(primes)
+
+    width = table.flatten.max.to_s.size + 2
+
+    puts "\n"
+    puts ([""] + primes).map { |i| i.to_s.rjust(width) }.join
+    puts ([""] + primes).map { "_".rjust(width) }.join
+
+    table.each_with_index do |a, indx|
+      print(primes[indx].to_s.rjust(width-2) + " |")
+
+      puts a.map { |i| print i.to_s.rjust(width) }.join
+    end
+    puts "\n"
+  end
+
+end
+
+
+PrimesMultTable.print_table(ARGV[0])
