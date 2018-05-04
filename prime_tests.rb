9290adb77dab7db0d4e6729fba30ad234a686358
menisy <elmenisy@gmail.com>
2018-05-04 04:16:03 +0200

    optimized the primes generator and added tests and bm for it

--- prime_tests.rb
+++ prime_tests.rb
@@ -11,6 +11,10 @@ class PrimesTests < Assert::Context
   def test_prime_generation
     arr = [10, 20, 50, 1000]
     arr.each do |i|
+      # assert primes are equal for naive solution
+      assert_equal PrimesGenerator.n_primes(i, false),
+                  Prime.first(i)
+      # assert primes are equal for optimized solution
       assert_equal PrimesGenerator.n_primes(i),
                   Prime.first(i)
     end
@@ -19,10 +23,14 @@ class PrimesTests < Assert::Context
 
   def test_benchmarks
     Benchmark.bm do |bm|
-      puts "My Prime Generator for n = 1000"
-      bm.report{PrimesGenerator.n_primes(1000)}
-      puts "Ruby's Prime Generator for n = 1000"
-      bm.report{Prime.first(1000)}
+      puts "Naive Prime Generator for n = 10000"
+      bm.report{PrimesGenerator.n_primes(10000, false)}
+
+      puts "Optimized Prime Generator for n = 10000"
+      bm.report{PrimesGenerator.n_primes(10000)}
+
+      puts "Ruby's Sieve Prime Generator for n = 10000"
+      bm.report{Prime.first(10000)}
     end
   end
 
--- primes_generator.rb
+++ primes_generator.rb
@@ -2,7 +2,15 @@ module PrimesGenerator
 
   FIRST_PRIME = 2
 
-  def self.n_primes(n=10)
+  def self.n_primes(n=10, optimized=true)
+    if optimized
+      n_primes_opt(n)
+    else
+      n_primes_naive(n)
+    end
+  end
+
+  def self.n_primes_naive(n=10)
     # seed primes array with the first prime
     primes = [FIRST_PRIME]
     # init i with first prime
@@ -17,8 +25,39 @@ module PrimesGenerator
       # skip if i is even
       next if i % 2 == 0
 
-      # check whether it's devisible by any prime
+      # check whether it's divisible by any prime
       for j in (0...primes.length)
+        if i % primes[j] == 0
+          is_prime = false
+          break
+        end
+      end
+
+      primes << i if is_prime
+    end
+    primes
+  end
+
+    def self.n_primes_opt(n=10)
+    # seed primes array with the first prime
+    primes = [FIRST_PRIME]
+    # init i with first prime
+    i = FIRST_PRIME
+
+    # loop until we reach the number of
+    # primes needed
+    while primes.length < n
+      i += 1
+      is_prime = true
+
+      # skip if i is even
+      next if i % 2 == 0
+
+      sqrt = Math.sqrt(i).to_i
+
+      # check whether it's divisible by any prime
+      # smaller than its square root
+      for j in (0...sqrt)
         if i % primes[j] == 0
           is_prime = false
           break
